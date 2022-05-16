bfe55454229d40dccad22d684c4798fcefbb62f8
Leonardo Z Facini <97568309+leozf13@users.noreply.github.com>
2022-05-16 00:15:20 -0300

    Add files via upload

---
+++ sintase-basica/src/Programa.java
@@ -0,0 +1,13 @@
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 6;
+		int soma = a + b;
+		
+		System.out.println("Resultado da soma é: "+ soma);
+		
+		}
+}
